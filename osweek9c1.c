0ffd2c6049913efd62e39b035e693d7b6ea61182
DIPALITIWARI <dipalitiwari437@gmail.com>
2025-11-14 12:19:59 +0530

    Add files via upload

---
+++ osweek9c1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+struct file{
+    char name[20];
+    int start,size,block[100];
+}f[100];
+int main(){
+int n,i,j;char search[20];
+printf("Enter number of files : ");
+scanf("%d",&n);
+for(i=0;i<n;i++){
+printf("Enter file %d name : ",i+1);
+scanf("%s",f[i].name);
+printf("Enter starting block of file %d : ",i+1);
+scanf("%d",&f[i].start);
+printf("Enter no of blocks in file %d : ",i+1);
+scanf("%d",&f[i].size);
+for(j=0;j<f[i].size;j++)
+f[i].block[j]=f[i].start+j;
+}
+printf("Enter the file name to be searched : ");
+scanf("%s",search);
+for(i=0;i<n;i++)
+if(strcmp(f[i].name,search)==0){
+printf("File Name %s Start block %d No. of blocks %d Blocks occupied\n",f[i].name,f[i].start,f[i].size);
+for(j=0;j<f[i].size;j++)
+printf("%d ",f[i].block[j]);
+return 0;}
+printf("File not found");
+}
+
